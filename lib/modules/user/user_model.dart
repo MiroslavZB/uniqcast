1f28e11c80b019449a922d4618e46e127cde5c0e
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2024-04-10 18:18:37 +0300

    added a way to format date time properly

--- lib/modules/user/user_model.dart
+++ lib/modules/user/user_model.dart
@@ -1,8 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:uniqcast/api/json_date_time.dart';
 import 'package:uniqcast/modules/authentication/login_response.dart';
 
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class UserModel {
   final int userId;
   final String operatorUid;
+  @JsonDateTimeUTC()
   final DateTime? tokenExpiryDate;
   final String? name;
   final int deviceId;
@@ -22,4 +28,8 @@ class UserModel {
         name: data.name,
         deviceId: data.deviceId,
       );
+
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
 }
