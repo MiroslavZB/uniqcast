1974f8caa9dfcb8c9fd93b2f82dac911e53c0432
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2024-04-10 18:21:11 +0300

    add user to storage

--- lib/utils/providers/storage_provider.dart
+++ lib/utils/providers/storage_provider.dart
@@ -1,22 +1,40 @@
 import 'package:get_storage/get_storage.dart';
+import 'package:uniqcast/modules/user/user_model.dart';
 
 abstract class StorageProvider {
+  static void logOut() {
+    setToken(null);
+    setExpiryDate(null);
+    setUser(null);
+  }
+
   static final _storage = GetStorage();
 
   // Keys
   static String get _tokenKey => 'token';
-  static String get _tokenExpiryDateKey => 'token';
+  static String get _tokenExpiryDateKey => 'token-expiry-date';
+  static String get _userKey => 'user';
 
   // Getters
   static String? get token => _storage.read(_tokenKey);
+
   static DateTime? get expiryDate {
-    final maybeDate = _storage.read(_tokenKey);
+    final maybeDate = _storage.read(_tokenExpiryDateKey);
     if (maybeDate == null) return null;
     return DateTime.parse(maybeDate).toLocal();
   }
 
+  static UserModel? get user {
+    final userJson = _storage.read(_userKey);
+    if (userJson == null) return null;
+    return UserModel.fromJson(userJson);
+  }
+
   // Setters
   static void setToken(String? newToken) => _storage.write(_tokenKey, newToken);
+
   static void setExpiryDate(DateTime? newTokenExpiryDate) =>
       _storage.write(_tokenExpiryDateKey, newTokenExpiryDate?.toUtc().toString());
+
+  static void setUser(UserModel? newUser) => _storage.write(_userKey, newUser?.toJson());
 }
