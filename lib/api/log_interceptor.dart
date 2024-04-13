201a23fb1b87a4aa80ff888bd6c658ae07dc6f84
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2024-04-13 10:43:44 +0300

    moved on error redirection to request interceptor

--- lib/api/log_interceptor.dart
+++ lib/api/log_interceptor.dart
@@ -1,12 +1,9 @@
 import 'dart:developer';
 import 'dart:math' hide log;
+import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:uniqcast/api/client.dart';
 import 'package:uniqcast/api/server_error.dart';
-import 'package:uniqcast/routing/router_provider.dart';
-import 'package:uniqcast/utils/providers/storage_provider.dart';
 
 enum LogLevel {
   /// No logs will be shown
@@ -24,8 +21,7 @@ enum LogLevel {
 
 class CustomLogInterceptor extends Interceptor {
   final LogLevel logLevel;
-  final ProviderRef<Client> ref;
-  const CustomLogInterceptor(this.logLevel, this.ref);
+  const CustomLogInterceptor(this.logLevel);
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
@@ -59,7 +55,9 @@ class CustomLogInterceptor extends Interceptor {
 
     final String message = 'Response Data<--- ${response.data}';
 
-    if (logLevel == LogLevel.basic) {
+    if (response.data is Uint8List) {
+      log('Response Data<---Uint8List (byte data)');
+    } else if (logLevel == LogLevel.basic) {
       log(message.substring(0, min(message.length, 500)));
     } else {
       log(message);
@@ -74,7 +72,7 @@ class CustomLogInterceptor extends Interceptor {
 
     log('Error---Response');
     if (err.response != null) {
-      log('Error---StatusCode<--- ${err.response!.statusCode} type<--- ${err.response!.data.runtimeType} '
+      log('Error---StatusCode<--- ${err.response!.statusCode} Type<--- ${err.response!.data.runtimeType} '
           '${err.response!.requestOptions.method.toUpperCase()} ${err.response!.requestOptions.uri}');
 
       final String message = 'Error---Data<--- ${err.response!.data}';
@@ -86,17 +84,6 @@ class CustomLogInterceptor extends Interceptor {
       }
 
       if (logLevel != LogLevel.basic) log('Error data---${err.response!.data}');
-
-      if (logLevel == LogLevel.full) {
-        log('Error---Headers---> ${err.response!.headers}');
-      } else {
-        log('Error---Authorization---> ${err.response!.headers['Authorization']}');
-      }
-
-      if (err.response!.statusCode == 401) {
-        StorageProvider.logOut();
-        ref.invalidate(routerProvider);
-      }
 
       return super.onError(err.copyWith(error: ServerError.fromJson(err.response!.data)), handler);
     } else {
