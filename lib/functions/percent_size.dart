c7fa39d929bd8939368db170351179ab4a5a09f5
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2024-04-13 10:40:09 +0300

    fixed an error with util func

--- lib/functions/percent_size.dart
+++ lib/functions/percent_size.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-double percentWidth(BuildContext context, [int x = 0]) {
+double percentWidth(BuildContext context, [int x = 100]) {
   if (x <= 0) return 0;
   final double width = MediaQuery.of(context).size.width;
   if (x >= 100) return width;
   return x * width * 0.01;
 }
 
-double percentHeight(BuildContext context, [int x = 0]) {
+double percentHeight(BuildContext context, [int x = 100]) {
   if (x <= 0) return 0;
   final double height = MediaQuery.of(context).size.height;
   if (x >= 100) return height;
