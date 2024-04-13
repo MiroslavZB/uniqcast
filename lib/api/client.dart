376253fe19815c18939c6a348dc6d6896d1f06fb
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2024-04-13 10:43:18 +0300

    semantic

--- lib/api/client.dart
+++ lib/api/client.dart
@@ -6,7 +6,7 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:uniqcast/api/factories.dart';
-import 'package:uniqcast/api/headers_interceptor.dart';
+import 'package:uniqcast/api/request_interceptor.dart';
 import 'package:uniqcast/api/log_interceptor.dart';
 import 'package:uniqcast/utils/extensions.dart';
 import 'package:uniqcast/utils/providers/storage_provider.dart';
